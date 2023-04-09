5f7fd135bdc911f386b1963020fe9a27c9d87d9e
Victor Brito <javibrito10@gmail.com>
2023-04-09 15:11:05 -0600

    page mobile layout done

--- src/app/components/menu/menu.component.ts
+++ src/app/components/menu/menu.component.ts
@@ -1,4 +1,4 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, Renderer2, ViewChild, ElementRef } from '@angular/core';
 import { IProduct } from 'src/app/models/product';
 import { ProductsService } from 'src/app/services/products.service';
 
@@ -10,9 +10,11 @@ import { ProductsService } from 'src/app/services/products.service';
 export class MenuComponent implements OnInit {
   public products!: IProduct[]
   public showCart = false
+  @ViewChild('menu') menu!: ElementRef
 
   constructor(
-    private _productService: ProductsService
+    private _productService: ProductsService,
+    private _renderer2: Renderer2
   ) {
   }
 
@@ -26,6 +28,15 @@ export class MenuComponent implements OnInit {
 
   getProducts(){
     this.products = this._productService.getProducts()
+  }
+
+  openMenu(): void {
+    console.log(this.menu)
+    this._renderer2.addClass(this.menu.nativeElement, 'menu-opened')
+  }
+
+  closeMenu(): void {
+    this._renderer2.removeClass(this.menu.nativeElement, 'menu-opened')
   }
 
 }
